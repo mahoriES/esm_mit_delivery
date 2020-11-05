96276c180581ba27945917a92a70c2aec0e8285a
Anushka <aagrawal6789@gmail.com>
2020-11-05 11:20:18 +0530

    crashylitics integration and modified urls

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:async_redux/async_redux.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:esamudaayapp/modules/AgentHome/view/my_home.dart';
@@ -7,12 +6,9 @@ import 'package:esamudaayapp/modules/AgentOrderDetail/view/order_detail.dart';
 import 'package:esamudaayapp/modules/Profile/views/profile_view.dart';
 import 'package:esamudaayapp/modules/accounts/views/accounts_view.dart';
 import 'package:esamudaayapp/modules/accounts/views/recommended_shop.dart';
-
 import 'package:esamudaayapp/modules/login/actions/login_actions.dart';
 import 'package:esamudaayapp/modules/login/views/login_View.dart';
-
 import 'package:esamudaayapp/modules/register/view/register_view.dart';
-
 import 'package:esamudaayapp/presentations/alert.dart';
 import 'package:esamudaayapp/presentations/check_user_widget.dart';
 import 'package:esamudaayapp/presentations/splash_screen.dart';
@@ -23,26 +19,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'modules/language/view/language_view.dart';
 import 'modules/otp/view/otp_view.dart';
+import 'services/crashylitics_delegate.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   NavigateAction.setNavigatorKey(navigatorKey);
+  runZonedGuarded(
+    () async {
+      runApp(EasyLocalization(
+        child: MyAppBase(),
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('ka', 'IN'),
+          Locale('ml', 'IN'),
+          Locale('ta', 'IN')
+        ],
+        path: 'assets/languages',
+      ));
+    },
+    (Object error, StackTrace stackTrace) {
+      debugPrint(
+          '********************************************** ${error.toString()}');
+      debugPrint('********************************************** $stackTrace');
 
-  runApp(EasyLocalization(
-    child: MyAppBase(),
-    supportedLocales: [
-      Locale('en', 'US'),
-      Locale('ka', 'IN'),
-      Locale('ml', 'IN'),
-      Locale('ta', 'IN')
-    ],
-    path: 'assets/languages',
-  ));
+      /// Whenever an error occurs, call the `recordError` function. This sends
+      /// Dart errors to crashlytics
+
+      CrashlyticsDelegate.recordError(error, stackTrace);
+    },
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -59,8 +69,8 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-    // TODO: implement initState
     PushNotificationsManager().init();
+    CrashlyticsDelegate.initializeCrashlytics();
     super.initState();
   }
 
